1d4eefc2f4ac53ca74ef3f32f430bb7ca9fd2f45
Ma27 <ma27-se@hotmail.com>
2015-04-07 22:41:40 +0200

    refactor product

--- src/ShopBundle/Service/ItemManager.php
+++ src/ShopBundle/Service/ItemManager.php
@@ -2,7 +2,33 @@
 
 namespace ShopBundle\Service;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 class ItemManager
 {
-	
+	/**
+	 * @var EntityManagerInterface
+	 */
+	private $em;
+
+	/**
+	 * @var \Doctrine\ORM\EntityRepository
+	 */
+	private $repository;
+
+	public function __construct(EntityManagerInterface $em)
+	{
+		$this->em         = $em;
+		$this->repository = $em->getRepository('ShopBundle:Product');
+	}
+
+	/**
+	 * Creates a list which contains all products which can be added to the shopping cart
+	 *
+	 * @return \ShopBundle\Entity\Product[]
+	 */
+	public function listProductItems()
+	{
+		return $this->repository->findAll();
+	}
 }
